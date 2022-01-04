9eb9cd341847d0fb5e07ce96ab619ea22a89489f
lizth93 <lizth93@hotmail.com>
2022-01-04 17:20:36 -0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0c06ba50-39aa-4a49-9dae-2ada02deb826';
+export const MODAL_CLOSE_SEC = 2;
